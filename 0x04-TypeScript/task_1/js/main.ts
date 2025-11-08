b5d4a814375a3a48ae7804920257b1531308461e
juskins <omojuwababatunde1@gmail.com>
2025-11-09 00:24:00 +0100

    Add StudentClass class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -15,11 +15,45 @@ interface printTeacherFunction {
     (firstName: string, lastName: string): string;
 }
 
-// const printTeacher: printTeacherFunction = (firstName, lastName) => {
-//     return `${firstName.charAt(0)}. ${lastName}`;
-// }
 
 function printTeacher(firstName: string, lastName: string): string {
   const firstInitial = firstName.charAt(0);
   return `${firstInitial}. ${lastName}`;
 }
+
+
+/**
+ * 1. Interface describing the shape of the StudentClass instance.
+ *    This defines the methods and properties instances will have.
+ */
+interface StudentInterface {
+  firstName: string;
+  lastName: string;
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentInterface;
+}
+
+
+class StudentClass implements StudentInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
