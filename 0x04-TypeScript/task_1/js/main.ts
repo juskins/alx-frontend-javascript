40416bfefcbd5ed9da7c0410d5413baf5f82e64a
juskins <omojuwababatunde1@gmail.com>
2025-11-09 00:05:03 +0100

    Add printTeacherFunction interface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -15,6 +15,6 @@ interface printTeacherFunction {
     (firstName: string, lastName: string): string;
 }
 
-const printTeacher: printTeacherFunction = (firstName, lastName) => {
+function printTeacher(firstName: string, lastName: string): string {
     return `${firstName.charAt(0)}. ${lastName}`;
 }
